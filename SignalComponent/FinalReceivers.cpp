afb4ecfbeb29677fde6b449f4e128b2c026aad73
Dmitry <dima.kaminskiy23.dk@gmail.com>
2024-06-04 22:09:22 +0300

    removed unnecessary components

--- SignalComponent/FinalReceivers.cpp
+++
@@ -1,6 +0,0 @@
-#include "FinalReceivers.h"
-#include <iostream>
-
-void HmiReceiver::processSignal(const std::string& signal) {
-    std::cout << "HMI processed: " << signal << std::endl;
-}
--- SignalComponent/FinalReceivers.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Interfaces.h"
-
-class HmiReceiver : public IFinalReceiver {
-public:
-    void processSignal(const std::string& signal) override;
-};
